0cfffaaff2919fa8f5209320632704a77c020f60
coldrain-f <intmain__@naver.com>
2022-09-22 22:53:19 +0900

    feat: 프로젝트 초기 환경 설정
    - TypeORM 설정 및 데이트베이스 연동
    - .env 환경변수 설정
    - gitignore 설정
    - Redis 설정
    - Swagger 설정

---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { CacheModule, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import * as Joi from 'joi';
+import { DatabaseModule } from './database/database.module';
+import * as redisStore from 'cache-manager-ioredis';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        SERVER_PORT: Joi.number().default(3000),
+        SERVER_MODE: Joi.string().default('dev'),
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+        DATABASE_USERNAME: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+        SWAGGER_USER: Joi.string().default('admin'),
+        SWAGGER_PASSWORD: Joi.string().default('admin'),
+        JWT_SECRET_KEY: Joi.string().default('secret'),
+      }),
+    }),
+    CacheModule.register({
+      store: redisStore,
+      host: '127.0.0.1',
+      port: 6379,
+    }),
+    DatabaseModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
