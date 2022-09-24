3b51235ae61c763a5bd653e87755b02f8ec95325
coldrain-f <intmain__@naver.com>
2022-09-24 20:29:33 +0900

    feat: hashtag entity 개발

--- src/hashtags/entities/hashtag.entity.ts
+++ src/hashtags/entities/hashtag.entity.ts
@@ -1,1 +1,20 @@
-export class HashTag {}
+import {
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class HashTag {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
