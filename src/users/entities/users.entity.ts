2f8a9bdd8072758f419d532e70c9c868dec68740
coldrain-f <intmain__@naver.com>
2022-09-24 20:24:11 +0900

    feat: user entity 개발

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -1,1 +1,23 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
