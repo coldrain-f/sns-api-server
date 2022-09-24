41b3ea34195be3d7d0b1c1d920b994cb40b37b7f
coldrain-f <intmain__@naver.com>
2022-09-24 20:26:37 +0900

    feat: board entity 개발

--- src/boards/entities/board.entity.ts
+++ src/boards/entities/board.entity.ts
@@ -1,1 +1,29 @@
-export class Board {}
+import {
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class Board {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  content: string;
+
+  @Column({ default: 0 })
+  views: number;
+
+  @Column({ default: 0 })
+  likeCount: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
