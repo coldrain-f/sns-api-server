0f31a7817fa88f6dabed6850c107223ea85942e0
coldrain-f <intmain__@naver.com>
2022-09-24 20:28:03 +0900

    feat: like entity 개발

--- src/likes/entities/like.entity.ts
+++ src/likes/entities/like.entity.ts
@@ -1,1 +1,16 @@
-export class Like {}
+import {
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class Like {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
