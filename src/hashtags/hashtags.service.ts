c575de1096a2eaf4a548fb8912d573cd54f3a69e
coldrain-f <intmain__@naver.com>
2022-09-26 16:38:07 +0900

    feat: 해시태그 저장 서비스 함수 구현

--- src/hashtags/hashtags.service.ts
+++ src/hashtags/hashtags.service.ts
@@ -43,7 +43,37 @@ export class HashtagsService {
     } catch (err) {
       await queryRunner.rollbackTransaction();
       throw new BadRequestException(
-        '게시글 번호에 해당하는 해시태그 삭제에 실패했습니다.',
+        '게시글 번호에 해당하는 해시태그 삭제를 실패했습니다.',
+      );
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  /**
+   * 게시글 번호와 해시태그 배열로 BoardHashtag 테이블에 저장
+   */
+  async saveAllByBoardId(boardId: number, hashtags: string[]) {
+    const queryRunner = this.connection.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const board: Board = await this.boardsService.getDetail(boardId);
+      hashtags.forEach(async (hashtag) => {
+        const savedHashtag: Hashtag = await this.hashtagsRepository.save({
+          name: hashtag,
+        });
+        await this.boardsHashtagsRepository.save({
+          board,
+          hashtag: savedHashtag,
+        });
+      });
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw new BadRequestException(
+        '게시글 번호에 해당하는 해시태그 저장을 실패했습니다.',
       );
     } finally {
       await queryRunner.release();
