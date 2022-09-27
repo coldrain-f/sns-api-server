7c61382434c49313204a0f5d8249e27916a831ea
coldrain-f <intmain__@naver.com>
2022-09-27 16:22:16 +0900

    chore: 사용하지 않는 import 제거

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,4 +1,4 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { BoardsController } from './boards.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
