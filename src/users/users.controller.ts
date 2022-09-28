e52a79b2188dc27b5d8ba835674d54dc663a59c7
coldrain-f <intmain__@naver.com>
2022-09-28 17:47:29 +0900

    feat: UsersController에 login handler 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,23 @@
 import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { AuthService, JwtTokenInfo } from 'src/auth/auth.service';
+import { LoginRequestDTO } from './dto/login-request.dto';
 import { SignupRequestDTO } from './dto/signup-request.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Post('signup')
   signup(@Body() request: SignupRequestDTO): Promise<number> {
     return this.usersService.signup(request);
+  }
+
+  @Post('login')
+  login(@Body() request: LoginRequestDTO): Promise<JwtTokenInfo> {
+    return this.authService.jwtLogin(request);
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/users.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [AuthModule, TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
