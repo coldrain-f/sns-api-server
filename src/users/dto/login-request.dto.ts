409289d7182daf0d93bf4a6a024b7a4fae2b48de
coldrain-f <intmain__@naver.com>
2022-10-06 18:05:26 +0900

    feat: UsersController 요청 Example Swagger 설정 추가

--- src/users/dto/login-request.dto.ts
+++ src/users/dto/login-request.dto.ts
@@ -1,11 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginRequestDTO {
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'olaf@naver.com',
+  })
   email: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'olaf',
+  })
   rawPassword: string;
 }
